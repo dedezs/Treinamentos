9c1aa472719a9924efd3efd0b9ce8fcd09e27377
Felipe Denani Ventaja <felipedenani@outlook.com>
2021-11-23 16:45:11 -0300

    c_UP

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
-$(document).ready(function() {
+function Incluir() {
+    var contador = 1
+    var inputTreinamento = document.getElementById('inputTreinamento').value
+    var inputCompanhia = document.getElementById('inputCompanhia').value
+    var Cidade = document.getElementById('Cidade').value
 
-    $("#btnSalvar").click(function() {
-        alert($('#warning').val());
+    $('#tabela').append(
+        (`<tr class="child">
+            <td>  ${inputTreinamento}  </td>
+            <td> ${inputCompanhia} </td>
+            <td> ${Cidade} </td>
+            `));
+    contador = contador + 1
+};
 
-        $('#warning').val('');
-    });
 
-});
+function Limpar() {
+    document.getElementById('inputTreinamento').value = '';
+    document.getElementById('inputCompanhia').value = '';
+    document.getElementById('Cidade').value = '';
+}
